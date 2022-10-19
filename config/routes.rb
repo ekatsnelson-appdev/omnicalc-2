3e92c2f8e09dc1061d1adf3bce54f158fd8816b4
ekatsnelson <elisek222@gmail.com>
2022-10-19 00:49:45 +0000

    Almost done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get("/add", { :controller => "application", :action => "blank_add_form" })
+  get("/wizard_add", { :controller => "application", :action => "calculate_add"})
+  
+  get("/subtract", { :controller => "application", :action => "blank_subtract_form" })
+  get("/wizard_subtract", { :controller => "application", :action => "calculate_subtract"})
+
+  get("/multiply", { :controller => "application", :action => "blank_multiply_form" })
+  get("/wizard_multiply", { :controller => "application", :action => "calculate_multiply"})
+  
+  get("/divide", { :controller => "application", :action => "blank_divide_form" })
+  get("/wizard_divide", { :controller => "application", :action => "calculate_divide"})
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
